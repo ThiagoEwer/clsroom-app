64493bed29ea488d7e7060d924dd461fd23941f9
thiagoewer <thiagoewer@gmail.com>
2024-09-12 23:57:56 +0000

    prgss

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,35 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+    var screenHeight = MediaQuery.of(context).size.height;
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
       home: Scaffold(
           appBar: AppBar(
-            title: const Text('CLASS ROOM APP'),
+            title:
+                const Text('Travelling'),
             backgroundColor: Colors.blue,
           ),
-          body: const Center(
-            child: Text("WELCOME TO CLASS ROOM APP"),
-          )),
+          body: Column(children: [
+            Stack(children: [
+              Image.network(
+                width: screenWidth,
+                fit: BoxFit.fitWidth,
+                  "https://vivaomundo.com.br/wp-content/uploads/2018/06/Grande-Barreira.jpg"),
+                  const Positioned (
+                    left: 10,
+                    bottom: 10,
+                    child: Column(
+                      children: [
+                        Text("Barreira de Corais", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
+                        Text("AUSTRÁLIA",style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),)
+                      ],
+                    ),
+                  )
+            ])
+          ])),
     );
   }
 }

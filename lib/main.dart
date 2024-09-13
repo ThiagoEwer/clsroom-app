eacf8aa195c8bba63ac7ab6e55979892b07f7265
thiagoewer <thiagoewer@gmail.com>
2024-09-13 00:17:35 +0000

    fix_prgss_img

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var screenWidth = MediaQuery.of(context).size.width;
-    var screenHeight = MediaQuery.of(context).size.height;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
@@ -18,24 +17,50 @@ class MyApp extends StatelessWidget {
                 const Text('Travelling'),
             backgroundColor: Colors.blue,
           ),
-          body: Column(children: [
-            Stack(children: [
-              Image.network(
-                width: screenWidth,
-                fit: BoxFit.fitWidth,
-                  "https://vivaomundo.com.br/wp-content/uploads/2018/06/Grande-Barreira.jpg"),
-                  const Positioned (
-                    left: 10,
-                    bottom: 10,
-                    child: Column(
-                      children: [
-                        Text("Barreira de Corais", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
-                        Text("AUSTRÁLIA",style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),)
-                      ],
-                    ),
-                  )
-            ])
-          ])),
+          body: SingleChildScrollView(
+            child: Column(children: [
+              Stack(children: [
+                Image.network(
+                  width: screenWidth,
+                  fit: BoxFit.fitWidth,
+                    "https://vivaomundo.com.br/wp-content/uploads/2018/06/Grande-Barreira.jpg"),
+                    const Positioned (
+                      left: 10,
+                      bottom: 10,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Barreira de Corais", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
+                          Text("AUSTRÁLIA",style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),)
+                        ],
+                      ),
+                    )
+              ]),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(onPressed: ()=>{}, icon: const Column(
+                    children: [
+                      Icon(Icons.favorite_border),
+                      Text("Favoritar")
+                    ],
+                  ),),
+                  IconButton(onPressed: ()=>{}, icon: const Column(
+                    children: [
+                      Icon(Icons.add),
+                      Text("Quero Ir")
+                    ],
+                  ),),
+                  IconButton(onPressed: ()=>{}, icon: const Column(
+                    children: [
+                      Icon(Icons.share),
+                      Text("Compartilhar")
+                    ],
+                  ),),
+                ],
+              )
+            ]),
+          )),
     );
   }
 }
